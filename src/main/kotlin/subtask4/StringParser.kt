d15cfa4379ecc80b30bd18c0aa095ad1f41a201b
hinkevich <51876492+hinkevich@users.noreply.github.com>
2020-03-29 22:58:11 +0300

    add task 4-5

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -2,8 +2,29 @@ package subtask4
 
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val array = inputString.toCharArray()
+        val result = ArrayList<String>()
+        var chek: Int
+        val start = charArrayOf('<', '[', '(')
+        val end = charArrayOf('>', ']', ')')
+        var count: Int
+        for (i in array.indices) {
+            chek = start.indexOf(array[i])
+            if (chek != -1) {
+                count = 0
+                var temp = ""
+                loop@ for (i1 in i + 1 until array.size) {
+                    if ((array[i1] == end[chek]) && (count == 0))
+                        break@loop
+                    temp += array[i1].toString()
+                    if (array[i1] == array[i]) count++
+                    if (array[i1] == end[chek]) count--
+                }
+                result.add(temp)
+            }
+        }
+        return result.toTypedArray()
     }
 }
+
--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -2,8 +2,35 @@ package subtask5
 
 class HighestPalindrome {
 
-    // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        val length = digitString.length
+        val max = getMax(digitString)
+        val stringBuilder = StringBuilder(digitString)
+        var count = 0
+
+        for (i in 0 until length / 2) {
+            if (digitString[i] != digitString[length - i - 1]) {
+                stringBuilder[i] = max
+                stringBuilder[length - i - 1] = max
+                count++
+            }
+        }
+        if (count > k) {
+            return (-1).toString()
+        }
+        return stringBuilder.toString()
+    }
+
+
+    private fun getMax(digitString: String): Char {
+        var max = Int.MIN_VALUE
+        for (digit in digitString) {
+            val i = digit.toInt()
+            if (i > max) {
+                max = i
+            }
+        }
+        return max.toChar()
     }
 }
+
